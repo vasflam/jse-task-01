d8327a6489870f34c580433ee08f3116b3cb434d
vasflam <vasflam@local>
2023-03-30 19:52:18 +0300

    Add teachers to stdents

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,23 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>(List.of(
-                new Student("Harry", "Potter", "potter@gmail.com", new Date(), 1),
-                new Student("John", "Travolta", "travolta@gmail.com", new Date(), 2),
-                new Student("Big", "Lebowsky", "lebowsky@gmail.com", new Date(), 2),
-                new Student("Low", "Level", "lebowsky@gmail.com", new Date(), 2),
-                new Student("High", "Man", "lebowsky@gmail.com", new Date(), 2)
-        ));
         List<Teacher> teachers = new ArrayList<>(List.of(
                 new Teacher("Obi", "VanKinobi", "kinobi@space.com", new Date(), TeacherGrade.Sensei),
                 new Teacher("hidden", "Mariarti", "kinobi@space.com", new Date(), TeacherGrade.Doctor),
                 new Teacher("God", "Person", "kinobi@space.com", new Date(), TeacherGrade.God)
         ));
 
+        List<Student> students = new ArrayList<>(List.of(
+                new Student("Harry", "Potter", "potter@gmail.com", new Date(), 1, teachers.get(0)),
+                new Student("John", "Travolta", "travolta@gmail.com", new Date(), 2, teachers.get(1)),
+                new Student("Big", "Lebowsky", "lebowsky@gmail.com", new Date(), 2, teachers.get(2)),
+                new Student("Low", "Level", "lebowsky@gmail.com", new Date(), 2, teachers.get(0)),
+                new Student("High", "Man", "lebowsky@gmail.com", new Date(), 2, teachers.get(2))
+        ));
+
         Random rand = new Random();
         for (Student s: students) {
-            int r = rand.nextInt(teachers.size());
-            Teacher t = teachers.get(r);
+            Teacher t = s.getTeacher();
             System.out.printf(
                 "Student: %s[gid=%d] was teached by %s [%s]\n",
                 s.getFullName(),
--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,12 @@ import java.util.Date;
 
 public class Student extends Person {
     private Integer gid;
+    private Teacher teacher;
 
-    public Student(String firstName, String lastName, String email, Date dob, Integer gid) {
+    public Student(String firstName, String lastName, String email, Date dob, Integer gid, Teacher t) {
         super(firstName, lastName, email, dob);
         this.gid = gid;
+        this.teacher = t;
     }
 
     public Integer getGid() {
@@ -14,5 +16,13 @@ public class Student extends Person {
 
     public void setGid(Integer gid) {
         this.gid = gid;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher t) {
+        this.teacher = t;
     }
 }
