51acfa80a42cbb481848fdcdcd87bc004d49aac6
vasflam <vasflam@local>
2023-03-30 19:21:27 +0300

    Fix students to 5

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         List<Student> students = new ArrayList<>(List.of(
                 new Student("Harry", "Potter", "potter@gmail.com", new Date(), 1),
                 new Student("John", "Travolta", "travolta@gmail.com", new Date(), 2),
-                new Student("Big", "Lebowsky", "lebowsky@gmail.com", new Date(), 2)
+                new Student("Big", "Lebowsky", "lebowsky@gmail.com", new Date(), 2),
+                new Student("Low", "Level", "lebowsky@gmail.com", new Date(), 2),
+                new Student("High", "Man", "lebowsky@gmail.com", new Date(), 2)
         ));
         List<Teacher> teachers = new ArrayList<>(List.of(
                 new Teacher("Obi", "VanKinobi", "kinobi@space.com", new Date(), TeacherGrade.Sensei),
                 new Teacher("hidden", "Mariarti", "kinobi@space.com", new Date(), TeacherGrade.Doctor),
                 new Teacher("God", "Person", "kinobi@space.com", new Date(), TeacherGrade.God)
-
         ));
 
-        int index = 0;
         for (Student s: students) {
-            Teacher t = teachers.get(index);
+            Random r = new Random();
+            Teacher t = teachers.get(r.nextInt(teachers.size()));
             System.out.printf(
                 "Student: %s[gid=%d] was teached by %s [%s]\n",
                 s.getFullName(),
@@ -26,7 +28,6 @@ public class Main {
                 t.getFullName(),
                 t.getGrade()
             );
-            index++;
         }
     }
 }
